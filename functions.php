d99c754216df5df8b043baf73a366669a3054e94
Corredora Online <135996211+Corredora-Online@users.noreply.github.com>
2024-05-20 14:53:18 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+// Aquí puedes añadir las funciones y características específicas de tu plugin
+
+// Ejemplo de una funcionalidad
+function corredora_online_custom_functionality() {
+    // Tu código aquí
+}
+
+add_action('init', 'corredora_online_custom_functionality');
+?>
