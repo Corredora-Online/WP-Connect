9b0d345615aff150c4ac920ede072d067c171bff
Corredora Online <135996211+Corredora-Online@users.noreply.github.com>
2024-05-21 18:08:24 -0400

    Update corredora-online.php

--- corredora-online.php
+++ corredora-online.php
@@ -11,13 +11,10 @@ GitHub Plugin URI: https://github.com/Corredora-Online/WP-Connect
 GitHub Branch: main
 */
 
-// Incluir el archivo de actualizaciones
 require_once plugin_dir_path(__FILE__) . 'updater.php';
 
-// Incluir el archivo de funcionalidades
 require_once plugin_dir_path(__FILE__) . 'functions.php';
 
-// Inicializar el actualizador
 if (class_exists('Corredora_Online_Updater')) {
     $updater = new Corredora_Online_Updater(__FILE__);
     $updater->initialize();
