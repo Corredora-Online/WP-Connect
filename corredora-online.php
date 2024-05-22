b2a255998aa3d5ac5529d437c65c1c560c1f6b0c
Corredora Online <135996211+Corredora-Online@users.noreply.github.com>
2024-05-22 14:50:34 -0400

    Update corredora-online.php

--- corredora-online.php
+++ corredora-online.php
@@ -3,7 +3,7 @@
 Plugin Name: Corredora Online
 Plugin URI: https://github.com/Corredora-Online/WP-Connect
 Description: Este plugin obtiene la información del sistema Corredora Online y procesa para ser mostrada en la página web.
-Version: 1.0.1
+Version: 1.0.2
 Author: Corredora Online
 Author URI: https://corredoraonline.com/
 License: GPL2
